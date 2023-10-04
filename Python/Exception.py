c465bdea0e30efc1550e399e8d1c26d255f0604a
jayaraman1120 <jayaraman1120@gmail.com>
2023-10-04 08:29:13 +0530

    after exception completion

--- Python/Exception.py
+++ Python/Exception.py
@@ -22,12 +22,26 @@ import os
 #
 #print ("Try block over")
 
+class TestException(Exception):
+    print("line1")
+    print("line")
+    pass
+
+
+
 try:
-    with open("new.txt",mode="r") as readfile:
-        pass
+    with open("./Test/json.txt",mode="r") as readfile:
+        a=int(input("enter value to print"))
+        raise TestException("Testing exception by raising this")
 except FileNotFoundError as error:
     print(error)
+    #raise IndexError raise another exception
+except TestException as error:
+    print(error)
+except Exception as error:
+    print(f"error {type(error).__name__}")
 else:
     print("Else block runs")
 finally:
     print("printing finally block")
+
---
+++ Python/filehandling.py
@@ -0,0 +1,4 @@
+#ref https://realpython.com/read-write-files-python/
+#ref https://realpython.com/working-with-files-in-python/
+
+
